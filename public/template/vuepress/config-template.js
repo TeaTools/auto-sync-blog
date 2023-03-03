eed823ed4425676336b4db76f10cc00856b80c48
茶博客 <68322136+tea-blog@users.noreply.github.com>
2023-03-03 18:43:03 +0800

    v0.0.1

--- public/template/vuepress/config-template.js
+++ public/template/vuepress/config-template.js
@@ -39,7 +39,7 @@ module.exports = {
     ],
   ],
   serviceWorker: true, // 是否开启 PWA
-  base: '/auto-sync-blog/', // 部署到github相关的配置
+  base: '/blog/', // 部署到github相关的配置
   markdown: {
     lineNumbers: true // 代码块是否显示行号
   },
