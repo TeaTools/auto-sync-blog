971d582922f47a188d18848c16c68572e8c9e4c4
MiyueSC <913784771@qq.com>
2024-04-18 17:32:13 +0800

    feat: 个人数据

--- configurations.js
+++ configurations.js
@@ -1,9 +1,9 @@
 export default {
   // blog 基础配置
   blog: {
-    title: "每日掘金",
+    title: "MiyueFE's blog",
     description: "专注于发掘站内优质创作者和优质内容",
-    logo: "https://p3-passport.byteacctimg.com/img/user-avatar/b72e991ee9b1c9bdca7b2bd4c8dc78a8~300x300.image",
+    logo: "https://p26-passport.byteacctimg.com/img/user-avatar/afcc0b5364cba06f7814368d1f44fc11~130x130.awebp",
     siteName: "tea.juejindev.com",
     head: [],
     keywords: ["前端", "后端", "移动端", "每日掘金", "酱酱的下午茶", "掘金", "掘金酱"],
@@ -12,18 +12,18 @@ export default {
 
   // vite press 构建配置
   press: {
-    name: "每日掘金",
-    text: "了解社区最新动态，发现最优质文章、最优质的你。",
-    tagline: "专注于发掘站内优质创作者和优质内容",
+    name: "MiyueFE",
+    text: "专注于 Web 前端的菜鸟开发",
+    tagline: "",
     image: "https://vitepress.dev/vitepress-logo-large.webp",
     actions: [
       { theme: "brand", text: "开始阅读", link: "/overview/index" },
-      { theme: "alt", text: "关注掘金", link: "https://juejin.cn/user/2819602825362840" },
+      { theme: "alt", text: "关注掘金", link: "https://juejin.cn/user/747323639208391" },
     ],
     features: [
-      { icon: "🎈", title: "博客平台小助手", details: "同步一流技术社区专属文章" },
-      { icon: "🎁", title: "数据分析小能手", details: "年月总览统计数据分析" },
-      { icon: "🎨", title: "自动化同步数据库", details: "每日自动化检查并更新" },
+      { icon: "🎈", title: "Vue", details: "" },
+      { icon: "🎁", title: "Bpmn", details: "" },
+      { icon: "🎨", title: "CSS", details: "" },
     ],
 
     // ["column", "category", "tag", "annual", "follow"]
@@ -40,7 +40,7 @@ export default {
     },
 
     // # https://vitepress.dev/zh/reference/default-theme-team-page
-    showTeam: true,
+    showTeam: false,
     members: [
       {
         avatar: "https://miyuefe.cn/assets/images/logo.svg",
@@ -113,7 +113,7 @@ export default {
 
   // 掘金
   juejin: {
-    userId: "2819602825362840",
+    userId: "747323639208391",
     columnsIds: [],
   },
 }
